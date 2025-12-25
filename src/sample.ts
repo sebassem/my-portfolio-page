401dad0c9fb92753b792856df3137659f68bad6e
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2025-12-25 16:10:29 +0200

    Update author details in sample.ts and enhance portfolio structure in Works.astro

--- src/sample.ts
+++ src/sample.ts
@@ -1,6 +1,6 @@
 const author = {
-    name: `Angela`,
-    nickname: `angie`,
+    name: `Seif Bassem`,
+    nickname: `seif`,
     tiktok: `https://www.tiktok.com/@codetonero`,
     ig: `https://www.instagram.com/codetonero/`,
     x: `https://x.com/codetonero`,
