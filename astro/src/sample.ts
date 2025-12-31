e131f217a452aac6951d5aa42cfd8937378580b3
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2025-12-31 21:19:10 +0200

    fix: comment out Service component in index.astro for layout adjustment fix: update author details in sample.ts for accuracy

--- astro/src/sample.ts
+++ astro/src/sample.ts
@@ -1,10 +1,10 @@
 const author = {
     name: `Seif Bassem`,
-    nickname: `seif`,
-    tiktok: `https://www.tiktok.com/@codetonero`,
-    ig: `https://www.instagram.com/codetonero/`,
-    x: `https://x.com/codetonero`,
-    email: 'mailto:anthonylannn@gmail.com'
+    nickname: `Seif Bassem`,
+    linkedin: `https://www.linkedin.com/in/seif-bassem/`,
+    github: `https://www.github.com/sebassem/`,
+    x: `https://x.com/sebassem`,
+    email: 'seif.bassem@outlook.com'
 
 }
 
